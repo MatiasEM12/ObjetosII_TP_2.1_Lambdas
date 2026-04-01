696edce305d1028e9b95e199d7c2af1677f576a7
MatiasEM12 <matiasemimellado@gmail.com>
2026-04-01 17:29:45 -0300

    Registro parael punto 4

---
+++ src/main/java/Refactorizacion_Punto4/Personas.java
@@ -0,0 +1,30 @@
+package Refactorizacion_Punto4;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Personas {
+
+    //filtra la lista de personas devolviendo otra lista con
+    //solo aquellas cuyo nombre comienza con E
+    public List<Persona> nombresQueEmpiezanConE(List<Persona> p) {
+        List<Persona> resultado = new ArrayList<>();
+        for (Persona persona : p) {
+            if (persona.nombre().startsWith("E")) {
+                resultado.add(persona);
+            }
+        }
+        return resultado;
+    }
+
+    public List<Persona> nombresCuyaCantidadDeLetrasEsPar(List<Persona> p) {
+        List<Persona> resultado = new ArrayList<>();
+        for (Persona persona : p) {
+            if (persona.nombre().length() % 2 == 0) {
+                resultado.add(persona);
+            }
+        }
+        return resultado;
+    }
+}
