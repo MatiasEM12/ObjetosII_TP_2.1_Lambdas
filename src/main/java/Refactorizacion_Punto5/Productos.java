833fcb35b66df1ef424da8d93b301841d71c3a1e
MatiasEM12 <matiasemimellado@gmail.com>
2026-04-03 11:02:49 -0300

    refactor Productos con lambda Supplier

--- src/main/java/Refactorizacion_Punto5/Productos.java
+++ src/main/java/Refactorizacion_Punto5/Productos.java
@@ -3,6 +3,7 @@ package Refactorizacion_Punto5;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Productos {
     public static final String SIN_PERMISOS = "No tiene los permisos necesarios";
@@ -14,25 +15,45 @@ public class Productos {
         this.productos = productos;
     }
 
-    public void addProducto(String userId, Producto producto) {
+/*
+* Puedo usar Supplier con 2 metetodos void y uno que devuelve una colleccion
+* ya que add y remove devuelven un booleano y se puede utilizar como generico T
+* */
+
+    private <T> T ejecutarConPermiso(String userId, Supplier<T> runnable ){
         if (!this.security.checkPermission(userId)) {
             throw new RuntimeException(SIN_PERMISOS);
         }
-        this.productos.add(producto);
+      return runnable.get();
+    }
+    public void addProducto(String userId, Producto producto) {
+
+       this.ejecutarConPermiso(userId,()->this.productos.add(producto));
+
+        /*if (!this.security.checkPermission(userId)) {
+            throw new RuntimeException(SIN_PERMISOS);
+        }
+        this.productos.add(producto);*/
     }
 
     public void removeProducto(String userId, Producto producto) {
-        if (!this.security.checkPermission(userId)) {
+
+        this.ejecutarConPermiso(userId,()->this.productos.remove(producto));
+
+        /*if (!this.security.checkPermission(userId)) {
             throw new RuntimeException(SIN_PERMISOS);
         }
-        this.productos.remove(producto);
+        this.productos.remove(producto);*/
     }
 
     public List<Producto> listAll(String userId) {
-        if (!this.security.checkPermission(userId)) {
+
+        return this.ejecutarConPermiso(userId, ()->Collections.unmodifiableList(this.productos));
+
+        /*if (!this.security.checkPermission(userId)) {
             throw new RuntimeException(SIN_PERMISOS);
         }
-        return Collections.unmodifiableList(this.productos);
+        return Collections.unmodifiableList(this.productos);*/
     }
 
     int cantidad() {
